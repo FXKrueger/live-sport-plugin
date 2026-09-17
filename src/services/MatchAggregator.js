3eec0b3d30cdc9ce16d9b6d0276baecd09d1a0b8
rajhodedara <68367766+rajhodedara@users.noreply.github.com>
2026-09-17 14:13:16 +0530

    chore: save local state before fork upgrade

--- src/services/MatchAggregator.js
+++ src/services/MatchAggregator.js
@@ -137,8 +137,8 @@ function _tryExtractTeams(title) {
 // ────────────────────────────────────────────────────────────────────────────
 
 class MatchAggregator {
-  constructor({ timStreamsProvider, watchFootyProvider, cdnLiveProvider, streamSports99Provider, streamicProvider, streamedPkProvider, cacheService, yamlProviders , replayzoneProvider}) {
-    this.providers = [timStreamsProvider, watchFootyProvider, cdnLiveProvider, streamSports99Provider, streamicProvider, streamedPkProvider, ...(yamlProviders || []), replayzoneProvider];
+  constructor({ timStreamsProvider, watchFootyProvider, cdnLiveProvider, streamSports99Provider, streamicProvider, streamedPkProvider, cacheService, yamlProviders , replayzoneProvider, iptvOrgProvider}) {
+    this.providers = [timStreamsProvider, watchFootyProvider, cdnLiveProvider, streamSports99Provider, streamicProvider, streamedPkProvider, ...(yamlProviders || []), replayzoneProvider, iptvOrgProvider];
     this.cacheService = cacheService;
   }
 
